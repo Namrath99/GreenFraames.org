94ded538d0f93dc281b653b4baf106fb893fcf6b
Namrath Lakkadi <namrathlakkadi@namraths-laptop.lan>
2025-03-11 00:09:26 -0400

    setup folder structures, installed tailwind,verified tailwind,git ignored unnecessary files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}" // This tells Tailwind to watch all HTML and JS files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
